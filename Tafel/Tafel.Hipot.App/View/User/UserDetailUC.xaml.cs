818bf1a65784aca81debe6f1db957195195f0a16
张建通 <jtzhang163@163.com>
2018-08-09 15:14:09 +0800

    登录窗体倒角

--- Tafel/Tafel.Hipot.App/View/User/UserDetailUC.xaml.cs
+++ Tafel/Tafel.Hipot.App/View/User/UserDetailUC.xaml.cs
@@ -16,14 +16,7 @@ namespace Tafel.Hipot.App.View
         {
             InitializeComponent();
             this.DataContext = AppCurrent.AppViewModel;
-            //gridUserDetail.DataContext = Current.User;
-        }
 
-
-        private void Hyperlink_Click(object sender, RoutedEventArgs e)
-        {
-            LoginWindow window = new LoginWindow();
-            window.ShowDialog();
         }
 
         private void hyberlinkLogout_Click(object sender, RoutedEventArgs e)
@@ -40,18 +33,7 @@ namespace Tafel.Hipot.App.View
                 AppCurrent.AppViewModel.CurrentUserNameTip = string.Empty;
                 Current.ShowTips(Current.User.Name + "成功注销");
 
-                //Thread t = new Thread(() =>
-                //{
-                //  Thread.Sleep(2000);
-                //  Dispatcher.Invoke(new Action(() =>
-                //  {
-                //    //登录成功，关闭窗口
-                //    this.Close();
                 AppCurrent.AppViewModel.IsLogin = false;
-                AppCurrent.AppViewModel.MainWindowsBackstageIsOpen = true;
-                //  }));
-                //});
-                //t.Start();
 
                 LoginWindow window = new LoginWindow();
                 window.Show();
@@ -68,32 +50,6 @@ namespace Tafel.Hipot.App.View
         private void hyberlinkManage_Click(object sender, RoutedEventArgs e)
         {
 
-        }
-    }
-
-    public class ShowLoginConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            return (bool)value ? Visibility.Hidden : Visibility.Visible;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-    public class ShowUserDetailConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            return (bool)value ? Visibility.Visible : Visibility.Hidden;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
         }
     }
 }
