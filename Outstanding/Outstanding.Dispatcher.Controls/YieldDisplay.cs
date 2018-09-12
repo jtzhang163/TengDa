e9188bd24c79bb63f34f0bf3ea1097a3f2c74fea
张建通 <jtzhang163@163.com>
2018-09-12 10:25:54 +0800

    产量优化

---
+++ Outstanding/Outstanding.Dispatcher.Controls/YieldDisplay.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Outstanding.Dispatcher.Controls
+{
+    public partial class YieldDisplay: UserControl
+    {
+        public YieldDisplay()
+        {
+            InitializeComponent();
+        }
+
+
+        public delegate void BtnClick(object sender, EventArgs e);
+
+        public event BtnClick btnYieldClearClick;
+
+        private void btnYieldClear_Click(object sender, EventArgs e)
+        {
+            if (btnYieldClearClick != null)
+            {
+                btnYieldClearClick(sender, new EventArgs());
+            }
+        }
+
+        public void YieldUpdate()
+        {
+            lbShowFeedingOK2.Text = Yield.FeedingOK.ToString();
+            lbShowBlankingOK2.Text = Yield.BlankingOK.ToString();
+        }
+
+        public void SetClearYieldTime(DateTime dateTime)
+        {
+            lbClearYieldTime.Text = dateTime.ToString("yyyy/M/d H:mm");
+        }
+    }
+}
---
+++ Outstanding/Outstanding.Dispatcher/Yield.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using TengDa;
+using TengDa.WF;
+
+namespace Outstanding.Dispatcher
+{
+    /// <summary>
+    /// 产量
+    /// </summary>
+    public class Yield : Service
+    {
+        #region 属性字段
+
+        private static string tableName = string.Empty;
+        public static string TableName
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(tableName))
+                {
+                    tableName = Config.DbTableNamePre + ".Yields";
+                }
+                return tableName;
+            }
+        }
+
+        /// <summary>
+        /// 夹具方向
+        /// </summary>
+        [DisplayName("夹具方向")]
+        public ClampOri ClampOri { get; set; } = ClampOri.未知;
+
+
+        private int feedingOK = 0;
+        /// <summary>
+        /// 上料良品数
+        /// </summary>
+        [DisplayName("上料良品数")]
+        public int FeedingOK
+        {
+            get { return feedingOK; }
+            set
+            {
+                if (feedingOK != value)
+                {
+                    UpdateDbField("FeedingOK", value);
+                }
+                feedingOK = value;
+            }
+        }
+
+
+        private int feedingNG = 0;
+        /// <summary>
+        /// 上料不良品数
+        /// </summary>
+        [DisplayName("上料不良品数")]
+        public int FeedingNG
+        {
+            get { return feedingNG; }
+            set
+            {
+                if (feedingNG != value)
+                {
+                    UpdateDbField("FeedingNG", value);
+                }
+                feedingNG = value;
+            }
+        }
+
+        private int blankingOK = 0;
+        /// <summary>
+        /// 下料良品数
+        /// </summary>
+        [DisplayName("下料良品数")]
+        public int BlankingOK
+        {
+            get { return blankingOK; }
+            set
+            {
+                if (blankingOK != value)
+                {
+                    UpdateDbField("BlankingOK", value);
+                }
+                blankingOK = value;
+            }
+        }
+
+
+        private int blankingNG = 0;
+        /// <summary>
+        /// 下料不良品数
+        /// </summary>
+        [DisplayName("下料不良品数")]
+        public int BlankingNG
+        {
+            get { return blankingNG; }
+            set
+            {
+                if (blankingNG != value)
+                {
+                    UpdateDbField("BlankingNG", value);
+                }
+                blankingNG = value;
+            }
+        }
+
+        /// <summary>
+        /// 备注
+        /// </summary>
+        [DisplayName("备注")]
+        public string Remark { get; set; } = string.Empty;
+
+        #endregion
+
+        #region 构造方法
+
+        public Yield() : this(-1) { }
+
+        public Yield(int id)
+        {
+            if (id < 0)
+            {
+                this.Id = -1;
+                return;
+            }
+
+            string msg = string.Empty;
+
+            DataTable dt = Database.Query("SELECT * FROM [dbo].[" + TableName + "] WHERE Id = " + id, out msg);
+
+            if (!string.IsNullOrEmpty(msg))
+            {
+                Error.Alert(msg);
+                return;
+            }
+
+            if (dt == null || dt.Rows.Count == 0) { return; }
+
+            Init(dt.Rows[0]);
+
+            //释放资源
+            dt.Dispose();
+        }
+
+        #endregion
+
+        #region 初始化方法
+        protected void Init(DataRow rowInfo)
+        {
+            if (rowInfo == null)
+            {
+                this.Id = -1;
+                return;
+            }
+
+            InitFields(rowInfo);
+        }
+
+        protected void InitFields(DataRow rowInfo)
+        {
+            this.Id = TengDa._Convert.StrToInt(rowInfo["Id"].ToString(), -1);
+            this.ClampOri = (ClampOri)Enum.Parse(typeof(ClampOri), rowInfo["ClampOri"].ToString());
+            this.FeedingOK = TengDa._Convert.StrToInt(rowInfo["FeedingOK"].ToString(), 0);
+            this.FeedingNG = TengDa._Convert.StrToInt(rowInfo["FeedingNG"].ToString(), 0);
+            this.BlankingOK = TengDa._Convert.StrToInt(rowInfo["BlankingOK"].ToString(), 0);
+            this.BlankingNG = TengDa._Convert.StrToInt(rowInfo["BlankingNG"].ToString(), 0);
+            this.Remark = rowInfo["Remark"].ToString();
+        }
+        #endregion
+
+        #region 列表
+        private static List<Yield> yieldList = new List<Yield>();
+        public static List<Yield> YieldList
+        {
+            get
+            {
+                if (yieldList.Count < 1)
+                {
+                    string msg = string.Empty;
+
+                    DataTable dt = Database.Query("SELECT * FROM [dbo].[" + TableName + "]", out msg);
+                    if (!string.IsNullOrEmpty(msg))
+                    {
+                        Error.Alert(msg);
+                        return null;
+                    }
+
+                    if (dt != null && dt.Rows.Count > 0)
+                    {
+                        for (int i = 0; i < dt.Rows.Count; i++)
+                        {
+                            Yield yield = new Yield();
+                            yield.InitFields(dt.Rows[i]);
+                            yieldList.Add(yield);
+                        }
+                    }
+                }
+                return yieldList;
+            }
+        }
+
+        #endregion
+    }
+}
