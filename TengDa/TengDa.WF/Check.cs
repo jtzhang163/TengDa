353116c9c325c28f3ab067f98ba8e8d5c17a777b
张建通 <jtzhang163@163.com>
2018-10-24 15:09:01 +0800

    添加

---
+++ TengDa/TengDa.WF/Check.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TengDa.WF
+{
+    /// <summary>
+    /// 检测
+    /// </summary>
+    public static class Check
+    {
+        /// <summary>
+        /// 检测是否登录，未登录弹窗提示
+        /// </summary>
+        /// <returns></returns>
+        public static bool Login()
+        {
+            var isLogin = Current.IsLogin;
+            if (!isLogin)
+            {
+                Tip.Alert("尚未登录!");
+            }
+            return isLogin;
+        }
+    }
+}
---
+++ TengDa/TengDa/OmronPLC.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TengDa
+{
+    public class OmronPLC
+    {
+        public static string GetBitStr(short val, int totalWidth)
+        {
+            return Convert.ToString(val, 2).PadLeft(totalWidth, '0');
+        }
+    }
+}
