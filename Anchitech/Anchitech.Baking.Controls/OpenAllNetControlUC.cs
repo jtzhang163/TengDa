1f27c97db4f860a99734d174eba3fd7d304af3c2
张建通 <jtzhang163@163.com>
2019-06-26 13:30:59 +0800

    打开全部网控

---
+++ Anchitech/Anchitech.Baking.Controls/OpenAllNetControlUC.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Anchitech.Baking.Controls
+{
+    /// <summary>
+    /// 打开全部网控
+    /// </summary>
+    public partial class OpenAllNetControlUC : UserControl
+    {
+        public OpenAllNetControlUC()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnOpenAllNetControl_Click(object sender, EventArgs e)
+        {
+            Current.ovens.ForEach(o =>
+            {
+                o.Floors.ForEach(f =>
+                {
+                    if (!f.IsNetControlOpen)
+                    {
+                        f.toOpenNetControl = true;
+                    }
+                });
+            });
+        }
+    }
+}
