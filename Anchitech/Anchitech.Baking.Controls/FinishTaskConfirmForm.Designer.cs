18e02cd2a496dc2bd035ff4a193ea87161c8bd45
张建通 <jtzhang163@163.com>
2019-06-27 14:47:31 +0800

    任务完成手动确认

---
+++ Anchitech/Anchitech.Baking.Controls/FinishTaskConfirmForm.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Anchitech.Baking.Controls
+{
+    partial class FinishTaskConfirmForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbPwd = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(68, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(304, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "确定机器人取放盘已经结束，但当前任务尚未完成？";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(70, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "输入密码：";
+            // 
+            // tbPwd
+            // 
+            this.tbPwd.Location = new System.Drawing.Point(154, 83);
+            this.tbPwd.Name = "tbPwd";
+            this.tbPwd.Size = new System.Drawing.Size(100, 21);
+            this.tbPwd.TabIndex = 2;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(288, 82);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 3;
+            this.btnOK.Text = "确认";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.BtnOK_Click);
+            // 
+            // FinishTaskConfirmForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 152);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.tbPwd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FinishTaskConfirmForm";
+            this.Text = "取放盘结束确认";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbPwd;
+        private System.Windows.Forms.Button btnOK;
+    }
+}
---
+++ Anchitech/Anchitech.Baking.Controls/FinishTaskConfirmForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TengDa.WF;
+
+namespace Anchitech.Baking.Controls
+{
+    public partial class FinishTaskConfirmForm : Form
+    {
+        public FinishTaskConfirmForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnOK_Click(object sender, EventArgs e)
+        {
+            if (this.tbPwd.Text.Trim() == DateTime.Now.ToString("HHmm"))
+            {
+                Current.option.TaskIsFinished = true;
+                Tip.Alert("确认任务结束OK！");
+            }
+            else
+            {
+                
+            }
+        }
+    }
+}
